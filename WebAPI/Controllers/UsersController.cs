e04fda43c604e79471b9bf12f7bd72d5efca3bcc
Yılmaz Şen <yilmazsen77@outlook.com>
2021-03-25 22:54:59 +0300

    WebApi implementation

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            var result = _userService.GetAll();
+            return result.Data;
+        }
+    }
+}
