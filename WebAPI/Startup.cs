c033dacafc63efff092f0c9ee037ac73941b35a3
Yılmaz Şen <yilmazsen77@outlook.com>
2021-03-27 22:15:46 +0300

    Autofac implementation

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,8 +30,8 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<IUserService, UserManager>();
-            services.AddSingleton<IUserDal, EfUserDal>();
+            //services.AddSingleton<IUserService, UserManager>();
+            //services.AddSingleton<IUserDal, EfUserDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
